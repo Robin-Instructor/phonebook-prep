22a7013cd6815d25898e3596744ec3c73b5b7d49
Robin-Instructor <robin@clarusway.com>
2023-08-30 19:08:36 +0000

    new

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "Robin-Instructor"
+}
+
+variable "git-token" {
+  default = "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"
+}
+
+variable "key-name" {
+  default = "robin"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us."
+}
